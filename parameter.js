eb5c095702f6115f6131d888437437fc23ae23de
sobuj <sobujahmed124@gmail.com>
2023-01-29 22:33:37 +0600

    multiple

--- parameter.js
+++ parameter.js
@@ -14,3 +14,11 @@ function add(num1, num2) {
   console.log('add :', num1, num2)
 }
 add(2, 3)
+
+// sum
+
+function sum(a, b, c, d) {
+  var add = a + b + c + d
+  console.log('summation is', add)
+}
+sum(2, 4, 5, 6)
