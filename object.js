088654e75e3cd227288f9e231d0323a5a4a1c735
sobuj <sobujahmed124@gmail.com>
2023-01-30 00:05:47 +0600

    object

---
+++ object.js
@@ -0,0 +1,21 @@
+// declaring objects
+
+var student = {
+  name: 'sobuj',
+  roll: 21064,
+  dept: 'cse',
+  gpa: 3.67,
+}
+// here student is object...name,roll,dept,gpa are called properties or key of object....all the given vlaues are called key values
+
+// another object
+var myPhone = {
+  brand: 'samsung',
+  price: 10000,
+  color: 'golden',
+}
+// viewing whole property of an object
+console.log(myPhone)
+
+// viewing specific property of an object
+console.log(myPhone.price)
