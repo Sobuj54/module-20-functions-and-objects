ed840ea964e531afd3ec1dcb9fee6540e217a2ee
sobuj <sobujahmed124@gmail.com>
2023-01-29 22:03:06 +0600

    function and parametr

---
+++ function.js
@@ -0,0 +1,10 @@
+// declaring function
+
+function fanOn() {
+  console.log('ami fan on korte parmu na.')
+}
+
+// calling function
+fanOn()
+console.log('kan parbi na')
+fanOn()
---
+++ parameter.js
@@ -0,0 +1,10 @@
+// declaring function with parameter
+
+function somosaAno(money) {
+  console.log('mama somosa dan')
+  console.log(money)
+}
+
+// calling function with giving value to the parameter
+
+somosaAno(100)
