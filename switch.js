2250d81d2aa4114ad72abe77cdf4747a07ba5c88
sobuj <sobujahmed124@gmail.com>
2023-01-30 19:57:15 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,25 @@
+// switch
+
+const color = 'green'
+
+switch (color) {
+  case 'blue':
+    console.log('u are my blue friend')
+    break
+
+  case 'dark':
+    console.log('u are my dark friend')
+    break
+
+  case 'yellow':
+    console.log('u are my yellow friend')
+    break
+
+  case 'green':
+    console.log('u are my green friend')
+    break
+
+  default:
+    console.log('u r not my friend')
+    break
+}
